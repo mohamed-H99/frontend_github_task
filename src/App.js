5af5259c0602fff27f2fcb62e23f11986e32706a
ahmed hashem <ahmdbhgat4@gmail.com>
2021-06-29 21:16:15 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+import Home from './views/Home'
+import Error from './views/Error'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/404">
+            <Error />
+          </Route>
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
